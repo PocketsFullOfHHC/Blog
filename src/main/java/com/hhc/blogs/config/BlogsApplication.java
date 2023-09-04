e10779f3b741e9ac440ed25cf34224f90ac3a506
HHC <HHC20000201@163.com>
2023-09-04 21:04:04 +0800

    第一天：后端初始化

--- src/main/java/com/hhc/blogs/config/BlogsApplication.java
+++ src/main/java/com/hhc/blogs/config/BlogsApplication.java
@@ -1,12 +1,13 @@
-package com.hhc.blogs;
+package com.hhc.blogs.config;
 
-import javafx.application.Application;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.hhc")
 @SpringBootApplication
 public class BlogsApplication {
 
