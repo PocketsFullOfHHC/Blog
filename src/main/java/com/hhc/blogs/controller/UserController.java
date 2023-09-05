5b93dc38ce933073b2e2ad6a99ad1d879039c402
HHC <HHC20000201@163.com>
2023-09-05 17:05:08 +0800

    第二天：后端初始化(3)：封装通用返回参数、类返回参数、请求参数，集成CopyUtils

--- src/main/java/com/hhc/blogs/controller/UserController.java
+++ src/main/java/com/hhc/blogs/controller/UserController.java
@@ -1,22 +1,28 @@
 package com.hhc.blogs.controller;
 
-import com.hhc.blogs.domain.User;
+import com.hhc.blogs.req.UserReq;
+import com.hhc.blogs.resp.CommonResp;
+import com.hhc.blogs.resp.UserResp;
 import com.hhc.blogs.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     @Resource
     private UserService userService;
 
-
-    @GetMapping("/user/list")
-    public List<User> list(){
-        return userService.list();
+    @GetMapping("/list")
+    public CommonResp<List<UserResp>> list(UserReq req){
+        CommonResp<List<UserResp>> resp = new CommonResp<>();
+        List<UserResp> list = userService.list(req);
+        resp.setContent(list);
+        return resp;
     }
 }
--- src/main/java/com/hhc/blogs/service/UserService.java
+++ src/main/java/com/hhc/blogs/service/UserService.java
@@ -1,7 +1,11 @@
 package com.hhc.blogs.service;
 
 import com.hhc.blogs.domain.User;
+import com.hhc.blogs.domain.UserExample;
 import com.hhc.blogs.mapper.UserMapper;
+import com.hhc.blogs.req.UserReq;
+import com.hhc.blogs.resp.UserResp;
+import com.hhc.blogs.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -13,7 +17,24 @@ public class UserService {
     @Resource
     private UserMapper userMapper;
 
-    public List<User> list(){
-        return userMapper.selectByExample(null);
+    public List<UserResp> list(UserReq req){
+        UserExample userExample = new UserExample();
+        UserExample.Criteria criteria = userExample.createCriteria();
+        criteria.andNameLike("%" + req.getName() + "%");
+        List<User> userList = userMapper.selectByExample(userExample);
+
+        // 循环写入封装的返回参数
+//        List<UserResp> respList = new ArrayList<>();
+//        for(User user : userList){
+//            // UserResp userResp = new UserResp();
+//            // BeanUtils.copyProperties(user, userResp);
+//            // 对象复制
+//            UserResp userResp = CopyUtil.copy(user, UserResp.class);
+//            respList.add(userResp);
+//        }
+
+        // 将User格式的list转化为UserResp格式的list并返回出去
+        List<UserResp> list = CopyUtil.copyList(userList, UserResp.class);
+        return list;
     }
 }
