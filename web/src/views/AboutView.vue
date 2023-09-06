3360f08c3a0ca8d905ab123c7f9629a0267024d5
HHC <HHC20000201@163.com>
2023-09-06 15:57:08 +0800

    第三天：博客前端初始化开发：解决axios跨域、找出ant固定导航的问题(其他组件需marginTop:64px)、Vue-cli多环境配置、集成打印前端日志、解决ant图标不显示的问题

--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div class="about" :style="{ padding: '0 50px', marginTop: '64px' }">
     <h1>This is an about page</h1>
     <h1>This is an about page</h1>
     <h1>This is an about page</h1>
