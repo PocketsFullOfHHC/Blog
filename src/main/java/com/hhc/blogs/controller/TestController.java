fa945d5962fec988f87c42c92ad7501d69bdd2e5
HHC <HHC20000201@163.com>
2023-09-11 20:31:37 +0800

    第八天：增加redis测试接口，注意获取redis数据时返回值为string

--- src/main/java/com/hhc/blogs/controller/TestController.java
+++ src/main/java/com/hhc/blogs/controller/TestController.java
@@ -2,15 +2,22 @@ package com.hhc.blogs.controller;
 
 import com.hhc.blogs.domain.Test;
 import com.hhc.blogs.service.TestService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class TestController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @Resource
     private TestService testService;
@@ -28,5 +35,19 @@ public class TestController {
     @GetMapping("/test/list1")
     public List<Test> list(){
         return testService.list();
+    }
+
+    @RequestMapping("/redis/set/{key}/{value}")
+    public String set(@PathVariable Long key, @PathVariable String value) {
+        redisTemplate.opsForValue().set(key, value, 3600, TimeUnit.SECONDS);
+        LOG.info("key: {}, value: {}", key, value);
+        return "success";
+    }
+
+    @RequestMapping("/redis/get/{key}")
+    public Object get(@PathVariable String key) {
+        Object object = redisTemplate.opsForValue().get(key);
+        LOG.info("key: {}, value: {}", key, object);
+        return object;
     }
 }
